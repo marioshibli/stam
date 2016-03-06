610523d2a24fc9256b89612e4d43b05fb108d0dc
Student <Student@10.0.11.93>
2016-03-06 14:56:22 +0200

    kkkkk

--- newa/src/newa/aaa.java
+++ newa/src/newa/aaa.java
@@ -1,6 +1,12 @@
 package newa;
 
 public class aaa {
+public static String Rec1(int n){
+	if (n==0)
+		return Integer.toString(n);
+	else 
+		return Integer.toString(n);
+}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
